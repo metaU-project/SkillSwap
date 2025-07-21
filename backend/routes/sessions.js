de92873ca791b3e2251b57c8357aa5d2c3212175
Wellington Mapise <wmapise@meta.com>
2025-07-21 11:20:51 -0700

    implement show user's sessions on profile tab - synced calendar with users upcoming sessions - added a container to show upcoming sessions and fallback if no sessions are scheduled

--- backend/routes/sessions.js
+++ backend/routes/sessions.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createSession } = require('../controllers/sessions.controller');
+const {
+  createSession,
+  getSessions,
+} = require('../controllers/sessions.controller');
 const checkAuth = require('../middleware/checkAuth');
+const { route } = require('./recommendation');
 
 router.post('/', checkAuth, createSession);
+router.get('/', checkAuth, getSessions);
 
 module.exports = router;
--- frontend/src/components/Profile/containers/CalendarContainer.jsx
+++ frontend/src/components/Profile/containers/CalendarContainer.jsx
@@ -2,12 +2,16 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
 
-const CalendarContainer = () => {
-  const [value, onChange] = useState(new Date());
+const CalendarContainer = ({ onDateSelect }) => {
+  const [value, setValue] = useState(new Date());
+  const handleChange = (date) => {
+    setValue(date);
+    if (onDateSelect) onDateSelect(date);
+  };
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar onChange={handleChange} value={value} />
     </div>
   );
 };
