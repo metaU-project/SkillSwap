115e169167e419320d0ffd18df91a939d51f1c0b
Wellington Mapise <wmapise@meta.com>
2025-08-04 16:44:58 -0700

    fix COR for mobile

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use(
     cookie: {
       secure: isProduction,
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: isProduction ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
